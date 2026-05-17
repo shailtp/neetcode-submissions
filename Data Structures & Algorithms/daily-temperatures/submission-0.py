bb00deef25cf60cfdb4e072efb14471747f6cc1a
Shail Patel <patelshail233@gmail.com>
2026-05-17 13:20:03 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #use a hashmap for storing index and element
+        n = len(temperatures)
+        res = []
+
+        for i in range(n):
+            count = 1
+            j = i + 1
+            while j < n:
+                if temperatures[j] > temperatures[i]:
+                    break
+                j += 1
+                count += 1
+            count = 0 if j == n else count
+            res.append(count)
+        return res
+
+        
+
+        
+     
+            
+
+    
+
+        
