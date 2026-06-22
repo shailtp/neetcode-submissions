dc334ee278ea0ce3500bf5b824e81f83b628b12a
Shail Patel <patelshail233@gmail.com>
2026-06-21 19:12:52 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        #dp problem, base cases n==1, n==2
+
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+        
+        dp=[0]*n
+        dp[0]=1
+        dp[1]=2
+
+        for i in range(2,n):
+            dp[i]=dp[i-1]+dp[i-2]
+
+        return dp[n-1]
+        
