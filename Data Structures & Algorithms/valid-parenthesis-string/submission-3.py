8932e8821f83d4e899068f48c85edfd6ba3e83d9
Shail Patel <patelshail233@gmail.com>
2026-09-12 11:42:41 -0700

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        #need 2 stacks of left and right, to store ( and * indexes
+        left = []
+        right = []
+
+
+        for i in range(len(s)):
+            if s[i]=='(':
+                left.append(i)
+
+            elif s[i]=='*':
+                right.append(i)
+
+            else:
+                #closing bracket encountered. check if left stack has elements
+                if not left and not right:
+                    return False
+                if left:
+                    left.pop()
+                else:
+                    right.pop()
+        while left and right:
+            if left.pop()>right.pop():
+                return False
+        
+        #no remaining opening brackets to be matched: valid sTring
+        return len(left)==0
